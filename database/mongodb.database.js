e9ff89e0635f12b483efc4f3acd148d2861889a0
unknown <amarjeet@baruzotech.com>
2023-01-23 12:29:02 +0530

    oauth2 with google auth library

---
+++ database/mongodb.database.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+mongoose.set('strictQuery', true);
+export const connectDatabase = () => {
+    console.log('mongodb is connecting');
+    mongoose
+      .connect(
+          process.env.MONGO_URI,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        }
+      )
+      .then(() => {
+        console.log(`mongodb is connected`);
+      })
+      .catch((err) => {
+        console.log(`${err}`);
+      });
+  };
---
+++ models/user.models.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+  },
+  refreshToken: {
+    type: String,
+  },
+  tokenExpiryDate: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export const findUser = async (id) => {
+  return await User.findOne({ googleId: id });
+};
+
+export const createUser = async(user)=>{
+    return await User.create(user)
+}
